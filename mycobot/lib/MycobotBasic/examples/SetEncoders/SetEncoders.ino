aed2ddf5bb656be47eee0f846e98880d8374ef5f
Pete <github@kthxbye.us>
2023-07-03 20:51:06 +0200

    Update mycobot lib

--- mycobot/lib/MycobotBasic/examples/SetEncoders/SetEncoders.ino
+++ mycobot/lib/MycobotBasic/examples/SetEncoders/SetEncoders.ino
@@ -1,26 +1,28 @@
-#include <MycobotBasic.h>
+#include <MyCobotBasic.h>
 #include <ParameterList.h>
 
-MycobotBasic myCobot;
+MyCobotBasic myCobot;
 
 Encoders encoders;
 
-void setup() {
-  // 打开Atom链接
-  myCobot.setup();
-  myCobot.powerOn();
-  Serial.begin(9600);
+void setup()
+{
+    // 打开Atom链接
+    myCobot.setup();
+    myCobot.powerOn();
+    Serial.begin(9600);
 }
-void loop() {
-  Serial.println("check to pos1...");
-  for(auto &val : encoders)
-    val = 2048;
-  myCobot.setEncoders(encoders, 50);
-  delay(5000);
-  
-  Serial.println("check to pos2...");
-  for(auto &val : encoders)
-    val = 1048;
-  myCobot.setEncoders(encoders, 50);
-  delay(5000);
+void loop()
+{
+    Serial.println("check to pos1...");
+    for (auto &val : encoders)
+        val = 2048;
+    myCobot.setEncoders(encoders, 50);
+    delay(5000);
+
+    Serial.println("check to pos2...");
+    for (auto &val : encoders)
+        val = 1048;
+    myCobot.setEncoders(encoders, 50);
+    delay(5000);
 }
